fd012a9edc7f64923fb14d41162aeb2d3912af20
Russell Chan <jrussellchan2000@gmail.com>
2022-01-23 21:30:56 +0700

    tuples

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,16 @@ let age: number;
 let ids: number[] = [1, 2, 3, 4];
 // ids.push('Hello') // Error
 let arr: any[] = [1, 'Str', false];
+
+/**
+ * Tuple
+ */
+let person: [number, string, boolean] = [1, 'Russell', false];
+// Tuple Array
+let employee: [number, string][];
+
+employee = [
+  [1, 'test'],
+  [2, 'test2'],
+  [3, 'test3'],
+];
