1f5b810627aa547a76da17b18b0d9a11762c544b
Russell Chan <jrussellchan2000@gmail.com>
2022-01-23 21:28:49 +0700

    initial setup and basic types

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+/**
+ * Basic Types
+ */
+let id = 5;
+let company = 'Russell Chan';
+let isPublished = true;
+let x = 'Hello';
+// x = false // Error
+let age;
+let ids = [1, 2, 3, 4];
+// ids.push('Hello') // Error
+let arr = [1, 'Str', false];
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Basic Types
+ */
+let id: number = 5;
+let company: string = 'Russell Chan';
+let isPublished: boolean = true;
+let x: any = 'Hello';
+// x = false // Error
+let age: number;
+
+let ids: number[] = [1, 2, 3, 4];
+// ids.push('Hello') // Error
+let arr: any[] = [1, 'Str', false];
